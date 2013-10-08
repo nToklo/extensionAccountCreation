55b87edc72ba5ea483e2108c7b93679167267993
Matt Smith <matt@ntoklo.com>
2013-10-08 12:55:42 +0100

    Styling updates and defensive code around event handler attachment for links.

--- includes/ntoklo.js
+++ includes/ntoklo.js
@@ -1,11 +1,19 @@
 window.nt_id = function (el) {
 	return document.getElementById(el);
 }
+window.nt_add_event = function ( obj, type, fn ) {
+	if ( obj.attachEvent ) {
+		obj['e'+type+fn] = fn;
+		obj[type+fn] = function(){obj['e'+type+fn]( window.event );}
+		obj.attachEvent( 'on'+type, obj[type+fn] );
+	} else {
+		obj.addEventListener( type, fn, false );
+	}
+}
 
 var 
 	launchRegister 	= window.nt_id("ntLaunchRegister"),
-	launchLogin 	= window.nt_id("ntLaunchLogin"),
-	platformInput 	= window.nt_id("platformInput");
+	launchLogin 	= window.nt_id("ntLaunchLogin");
 
 	window.createFrame = function (ev, loginOrRegister) {
 		var 
@@ -14,11 +22,11 @@ var
 			myFrame.id 	= "ntIFrameEl";
 			myFrame.src = "https://console.ntoklo.com/register" +
 						  "?p=" + window.ntParams.p + 
-						  "&f=" + window.ntParams.f +
-						  "&l=" + window.ntParams.l +
-						  "&e=" + window.ntParams.e +
-						  "&n=" + window.ntParams.n +
-						  "&d=" + window.ntParams.d +
+						  "&f=" + ((typeof window.ntParams.f !== "undefined") ? window.ntParams.f : "") +
+						  "&l=" + ((typeof window.ntParams.l !== "undefined") ? window.ntParams.l : "") +
+						  "&e=" + ((typeof window.ntParams.e !== "undefined") ? window.ntParams.e : "") +
+						  "&n=" + ((typeof window.ntParams.n !== "undefined") ? window.ntParams.n : "") +
+						  "&d=" + ((typeof window.ntParams.d !== "undefined") ? window.ntParams.d : "") +
 						  "&r=" + loginOrRegister;
 
 			window.nt_id("ntIFrameWrapper").className = loginOrRegister;
@@ -32,19 +40,22 @@ var
 			}
 	}
 
-	launchRegister.addEventListener("click", function (ev) {
+	if (launchRegister) {
+		window.nt_add_event(launchRegister, "click", function (ev) {
 			window.removeFrame();
 			window.createFrame(ev, "register");
 			ev.stopPropagation();
 			ev.preventDefault();
 			return false;
-	}, false);
+		});
+	}
 
-	launchLogin.addEventListener("click", function (ev) {
+	if (launchLogin) {
+		window.nt_add_event(launchLogin, "click", function (ev) {
 			window.removeFrame();
 			window.createFrame(ev, "login");
 			ev.stopPropagation();
 			ev.preventDefault();
 			return false;
-	}, false);
-
+		});
+	}
